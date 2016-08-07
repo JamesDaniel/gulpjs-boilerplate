06485785148ad48ac5128e353a437b0fccf66fc2
James McGarr <james.d.mcgarr@students.ittralee.ie>
2016-08-07 23:51:24 +0100

    added gulp task to compile .less to .css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 
+var less = require('gulp-less');
+var path = require('path');
+
 gulp.task('default', function() {
 	console.log('hello gulp');
 });
@@ -16,4 +19,12 @@ gulp.task('minify-js', function() {
   return gulp.src('scripts/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('compile-less', function() {
+    return gulp.src('./less/example.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./css/'));
 });
